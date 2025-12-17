49a361802f8661855e5585f1a3db1db3be64bf36
mario-ak37 <alfredkipkogei@proton.me>
2025-12-17 17:38:28 +0300

    exporting orders to CSV files

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,12 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.utils.safestring import mark_safe
-from unfold.admin import ModelAdmin, StackedInline, TabularInline
+from unfold.admin import ModelAdmin, TabularInline
+from unfold.decorators import action
 
 from .models import Order, OrderItem
 
@@ -41,3 +47,34 @@ class OrderAdmin(ModelAdmin):
 
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = ["export_to_csv"]
+
+    @action(description="Export to CSV", icon="file_download")
+    def export_to_csv(self, request: HttpRequest, queryset: QuerySet):
+        opts = self.model._meta
+        content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = content_disposition
+
+        writer = csv.writer(response)
+        # print(f"The fields look like this: {opts.get_fields()}")
+        fields = [
+            field
+            for field in opts.get_fields()
+            if not field.many_to_many and not field.one_to_many
+        ]
+
+        # Write a first row with header information
+        writer.writerow([field.verbose_name for field in fields])
+
+        # write data rows
+        for obj in queryset:
+            data_row = []
+
+            for field in fields:
+                value = getattr(obj, field.name)
+                if isinstance(value, datetime.datetime):
+                    value = value.strftime("%d/%m/%Y")
+                data_row.append(value)
+            writer.writerow(data_row)
+        return response
